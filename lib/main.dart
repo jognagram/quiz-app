39c2a3a9505cf96b03ee7b3034735328754ded0c
jogna <jordan.gnamien@smile.ci>
2023-10-03 11:18:08 +0000

    -- Jogna Git Commit --

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
 
 void main() {
   runApp(QuizzApp());
@@ -28,6 +29,25 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+
+  List<bool> answers = [false, true, true];
+
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
+  ];
+
+  // List<String> questions = [
+  //   'You can lead a cow down stairs but not up stairs. ',
+  //   'Approximately one quarter of human bones are in the feet.',
+  //   'A slug\'s blood is green.'
+  // ];
+
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -38,7 +58,7 @@ class _QuizPageState extends State<QuizPage> {
           flex: 5,
           child: Center(
             child: Text(
-              'This is where the question text will go',
+              '',
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 28.0,
@@ -51,7 +71,20 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: const EdgeInsets.all(15.0),
             child: TextButton(
-              onPressed: () {},
+              onPressed: () {
+                //the user picked true
+                bool correctAnswer = answers[questionNumber];
+
+                if (correctAnswer == true) {
+                  print('user got it right!');
+                } else {
+                  print('user got it wrong!');
+                }
+                setState(() {
+                  questionNumber++;
+                });
+                print(questionNumber);
+              },
               style: TextButton.styleFrom(
                 backgroundColor: Colors.green,
               ),
@@ -79,6 +112,9 @@ class _QuizPageState extends State<QuizPage> {
               ),
             ),
           ),
+        ),
+        Row(
+          children: scoreKeeper,
         )
       ],
     );
